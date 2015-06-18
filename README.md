0e3f6fada2f6a7b108510ebcf808eb4f070b539b
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-06-18 23:27:56 +0430

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# composer-installer
+# Composer Installer
 Composer Installer for RadPHP Framework
---
+++ src/BundleInstaller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rad\Composer;
+
+/**
+ * Composer Bundle Installer
+ *
+ * @package Rad\Composer
+ */
+class BundleInstaller
+{
+
+}
