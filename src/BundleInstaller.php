ff3e13a4c14720cd1a78f0fb202d5d6fbebfa3b4
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-06-19 12:23:30 +0430

    Added "supports" & "getInstallPath" methods

--- src/BundleInstaller.php
+++ src/BundleInstaller.php
@@ -3,6 +3,8 @@
 namespace Rad\Composer;
 
 use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+use Exception;
 
 /**
  * Composer Bundle Installer
@@ -11,5 +13,26 @@ use Composer\Installer\LibraryInstaller;
  */
 class BundleInstaller extends LibraryInstaller
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($packageType)
+    {
+        return 'radphp-bundle' === $packageType;
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        if (count(explode('-', $package->getPrettyName())) < 2) {
+            throw new Exception('Bundle names should be like "NAME-bundle"');
+        }
+
+        $name = str_replace('-bundle', '', $package->getPrettyName());
+        $name = str_replace(' ', '', ucwords(str_replace('-', '', $name)));
+
+        return "bundles" . DIRECTORY_SEPARATOR . $name;
+    }
 }
